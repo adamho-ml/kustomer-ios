82e955d17be94936df89c72e19b761a36b065eae
Adam Ho <79440027+adamho-ml@users.noreply.github.com>
2023-03-24 13:57:11 +0800

    demo: showKbArticle does not work on first launch

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -6,10 +6,13 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
-  
+
   @IBAction func showSupport(){
     Kustomer.show()
   }
 
-}
+  @IBAction func showKbArticle(){
+    Kustomer.showKbArticle(id: "YOUR_ARTICLE_ID_HERE")
+  }
 
+}
